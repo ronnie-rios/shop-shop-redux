d205735ddabb75c8711cb66682e4d1a7e5023db2
Ronnie Rios <ronniesmail14@gmail.com>
2021-08-28 13:39:36 -0500

    client build

---
+++ client/build/precache-manifest.2fd7b0e695db9b1d911c130c64937797.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e38f623bd0cc9d6b46be41dcf4ecebc8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cdc64b69c3bc727b8d0d",
+    "url": "/static/css/main.e213c893.chunk.css"
+  },
+  {
+    "revision": "911d86c5348db14a1b44",
+    "url": "/static/js/2.9e3448b2.chunk.js"
+  },
+  {
+    "revision": "aa4100970f46d7e0ec2af84de3f2740b",
+    "url": "/static/js/2.9e3448b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cdc64b69c3bc727b8d0d",
+    "url": "/static/js/main.94c6afb4.chunk.js"
+  },
+  {
+    "revision": "02751e160f988a50d44f",
+    "url": "/static/js/runtime-main.8a258a78.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
