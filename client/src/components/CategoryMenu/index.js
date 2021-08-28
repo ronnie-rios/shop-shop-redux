9cd3b054c9d89358088325c12148ad3db9b62446
Ronnie Rios <ronniesmail14@gmail.com>
2021-08-28 09:27:52 -0500

    refactored category menu

--- client/src/components/CategoryMenu/index.js
+++ client/src/components/CategoryMenu/index.js
@@ -1,27 +1,33 @@
-import React, { useEffect } from 'react';
+import React, { useEffect } from "react";
 import { useQuery } from '@apollo/react-hooks';
-import { useStoreContext } from '../../utils/GlobalState';
-import { UPDATE_CATEGORIES, UPDATE_CURRENT_CATEGORY } from '../../utils/actions';
-import { QUERY_CATEGORIES } from '../../utils/queries';
-import { idbPromise } from '../../utils/helpers';
+import { QUERY_CATEGORIES } from "../../utils/queries";
+import { UPDATE_CATEGORIES, UPDATE_CURRENT_CATEGORY } from "../../utils/actions";
+import { idbPromise } from "../../utils/helpers";
+import { useDispatch, useSelector } from 'react-redux';
 
 function CategoryMenu() {
-  const [state, dispatch] = useStoreContext();
+  const state = useSelector((state) => {
+    return state;
+  });
+  const dispatch = useDispatch();
 
   const { categories } = state;
-
   const { loading, data: categoryData } = useQuery(QUERY_CATEGORIES);
 
+  // Update the state with the categories upon page load or change
   useEffect(() => {
+    // if categoryData exists or has changed from the response of useQuery, then run dispatch to update the state with the UPDATE_CATEGORIES action
     if (categoryData) {
       dispatch({
         type: UPDATE_CATEGORIES,
         categories: categoryData.categories
       });
-      categoryData.categories.forEach(category => {
-        idbPromise('categories', 'put', category);
-      });
+    // also store the category data in IndexedDB
+    categoryData.categories.forEach(category => {
+      idbPromise('categories', 'put', category);
+    });
     } else if (!loading) {
+      // if the user is offline, load data from IndexedDB
       idbPromise('categories', 'get').then(categories => {
         dispatch({
           type: UPDATE_CATEGORIES,
@@ -29,7 +35,7 @@ function CategoryMenu() {
         });
       });
     }
-  }, [categoryData, loading, dispatch]);
+  }, [categoryData, loading, dispatch])
 
   const handleClick = id => {
     dispatch({
